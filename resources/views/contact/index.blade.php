aafd9e655dbe1b14f7fbfa7646e4373989544365
Str3ltsov <andtaress@live.com>
2023-09-18 14:17:39 +0300

    Session success, error, warning and info messages

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <div class="container py-5">
+        @include('messages')
         <div class="px-4 px-sm-0">
             <h2 class="mb-4">{{ __('Contact') }}</h4>
             @include('contact.form')
